04007020b5a9aaee206f0f7aad59b79014eaa4bd
elias gabriel kim <elias gabriel kim>
2017-06-05 19:03:35 -0300

    Arregle un temita que rompi antes sin querer

--- src/main/java/proyectoInversiones/indicadores/IndicadorPredefinido.java
+++ src/main/java/proyectoInversiones/indicadores/IndicadorPredefinido.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class IndicadorPredefinido {
+public class IndicadoresPredefinidos {
 	
 	NuevoLeerArchivo archivo = new NuevoLeerArchivo();
 	Collection<Empresa> empresas = archivo.leerArchivo();
@@ -25,20 +25,46 @@ public class IndicadorPredefinido {
 	public void setCuentas(List<Cuenta> cuentas) {
 		this.cuentas = cuentas;
 	}
-	public IndicadorPredefinido(){
-		
-	}
+
 	public void inicializarCuentas(){
-		
+		int i = 0;
+		List<Cuenta> asf = new ArrayList<Cuenta>();
 		for(Empresa head:empresas){
-			cuentas.addAll(head.getCuentas());
+			asf = head.getCuentas();
+			cuentas.addAll(i, asf);;
 		}
 		
 	}
+	public boolean validarEmpresa(String empresa){
+		
+		for(Empresa head:empresas){
+			if(head.getNombre().contains(empresa)){ 
+				return true;
+				}
+			}
+			return false;
+	}
+	public int calcularIngNeto(String empresa, int periodo){ //INDICADOR PREDEFINIDO POR EL USUARIO
+		int ingNeto = 0;
+		
+			if(validarEmpresa(empresa))
+				this.inicializarCuentas();
+			for(Cuenta head2:cuentas){
+				if(head2.getPeriodo() == periodo){
+					ingNeto = head2.getIngNetoOpCont() + head2.getIngNetoOpDiscont();
+				}
+			}
+	
+		return ingNeto;
+	} 
 	
 	public static void main (String args[]){
-		IndicadorPredefinido pre = new IndicadorPredefinido();
+		IndicadoresPredefinidos pre = new IndicadoresPredefinidos();
+		System.out.println(pre.getEmpresas());
 		pre.inicializarCuentas();
 		System.out.println(pre.getCuentas());
+		String amer = "America Movil";
+		
+		System.out.print(pre.calcularIngNeto(amer, 2006));
 	}
 }
