48fd7bd233fd92fee41889051573df3d2b4555ac
tgullon <tomasg.97@hotmail.com>
2017-10-10 19:07:10 -0300

    cambio minimo no funcional

---
+++ src/main/java/proyectoInversiones/servlet/Login.java
@@ -0,0 +1,31 @@
+package proyectoInversiones.servlet;
+
+import proyectoInversiones.Spring.LoginRegistry;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/login"})
+public class Login extends HttpServlet{
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        String token = LoginRegistry.getInstance().login(username, password);
+
+        if (token != null) {
+            resp.getOutputStream().print(token);
+        } else {
+            throw new ServletException("Usuario o contraseña invalidos.");
+        }
+
+    }
+
+
+	
+}
