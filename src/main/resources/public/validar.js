bb888c8357ae963ac08ed2ac12fd0db61cb5e90c
tgullon <tomasg.97@hotmail.com>
2017-12-13 19:53:31 -0300

    Cambios en UI (in progress)

--- src/main/resources/public/validar.js
+++ src/main/resources/public/validar.js
@@ -1,3 +1,5 @@
+var empresaSeleccionada;
+
 function validar(){
 	var empresa;
 	empresa=document.getElementById("Empresa").value;
@@ -6,4 +8,22 @@ function validar(){
 		alert("Ingrese un nombre de empresa valido; los registrados hasta el momento son America Movil, General Electric y Berkshire Hathaway");
 		return false;
 	}
+//	var TextoEncabezado = document.getElementById("TituloEmpresa").innerHTML= "<tr>"+Empresa+"</tr>";
+//	var Encabezado = document.createElement("TR");
+//	Encabezado.innerHTML=TextoEncabezado;
+//    document.getElementById("tablita").appendChild(Encabezado);
 }
+
+
+//function mostrarEmpresa(){
+//	var empresa = document.getElementById("Empresa").value;
+//	return empresa;
+//}
+//
+//function mostarEmpresaSeleccionada(){
+//	//var empresa = mostrarEmpresa();
+//	
+//	var empresa = document.getElementById("tabla").rows[0].cells;
+//	empresa[0].innerHTML = document.getElementById("Empresa").value;
+//	
+//}
