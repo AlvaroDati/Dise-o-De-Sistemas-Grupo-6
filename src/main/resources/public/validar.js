e205a9adff42600f8ac10cf77031982ab8013474
AlvaroDati <alvarodati@hotmail.com>
2017-12-18 01:51:16 -0300

    modificacion ui metodologia y cambios a demas html

--- src/main/resources/public/validar.js
+++ src/main/resources/public/validar.js
@@ -5,25 +5,9 @@ function validar(){
 	empresa=document.getElementById("Empresa").value;
 
 	if((empresa != "America Movil") && (empresa != "General Electric") && (empresa != "Berkshire Hathaway")){
-		alert("Ingrese un nombre de empresa valido; los registrados hasta el momento son America Movil, General Electric y Berkshire Hathaway");
+		alert("ERROR: Ingrese un nombre de empresa válido; los registrados hasta el momento son America Movil, General Electric y Berkshire Hathaway");
 		return false;
-	}
-//	var TextoEncabezado = document.getElementById("TituloEmpresa").innerHTML= "<tr>"+Empresa+"</tr>";
-//	var Encabezado = document.createElement("TR");
-//	Encabezado.innerHTML=TextoEncabezado;
-//    document.getElementById("tablita").appendChild(Encabezado);
+	}else
+		alert("Datos ingresados correctamente, se mostrará a continuación la informacion de la empresa: "+empresa);
+		return false;
 }
-
-
-//function mostrarEmpresa(){
-//	var empresa = document.getElementById("Empresa").value;
-//	return empresa;
-//}
-//
-//function mostarEmpresaSeleccionada(){
-//	//var empresa = mostrarEmpresa();
-//	
-//	var empresa = document.getElementById("tabla").rows[0].cells;
-//	empresa[0].innerHTML = document.getElementById("Empresa").value;
-//	
-//}
