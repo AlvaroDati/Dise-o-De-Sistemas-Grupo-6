c6edd916d28123126f7cd0d516f0e2bb916d2860
AlvaroDati <alvarodati@hotmail.com>
2017-12-18 03:16:22 -0300

    Agregué las pestañas faltantes, y reacomode el codigo, falta mergear

--- src/main/resources/public/validar.js
+++ src/main/resources/public/validar.js
@@ -11,3 +11,16 @@ function validar(){
 		alert("Datos ingresados correctamente, se mostrará a continuación la informacion de la empresa: "+empresa);
 		return false;
 }
+
+function validarMetodologia(){
+	var metodologia;
+	var metodologiasCargadas = ["Metodologia1","Metodologia2","Metodologia3"];
+	metodologia=document.getElementById("Metodologia").value;
+	if(metodologiasCargadas.find(metodologia)){
+		alert("ERROR: Ingrese una metodologia existente");
+		return false;
+	}else{
+		alert("Datos ingresados correctamente, se aplicará ahora dicha metodología a todas las empresas:");
+		return false;
+	}
+}
